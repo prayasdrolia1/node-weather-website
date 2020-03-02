8c43ed3e7efd1da6499302f6c1cd9201d68f9f16
Mohit <prayasdrolia@gmail.com>
2020-03-02 14:01:18 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -30,7 +30,8 @@ const forecast = (latitude, longitude, callback) => {
                 // summary: response.body.daily.data[0].summary,
                 //temp = response.body.currently.temperature,
                 // rainProb = response.body.currently.precipProbability
-                body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees Fahrenheit out. There is a " + body.currently.precipProbability + "% chance of rain."
+                
+                body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees Fahrenheit out. The high tody is " + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + ". There is a " + body.currently.precipProbability + "% chance of rain."
             )
         }
     })
